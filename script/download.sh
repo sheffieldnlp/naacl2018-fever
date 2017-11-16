3483666598bcd0cb63409cc4e75e7d904587569f
James Thorne <james@jthorne.co.uk>
2017-11-16 12:01:53 +0000

    added scripts to index/download from s3 bucket

---
+++ script/download.sh
@@ -0,0 +1,10 @@
+#$ -N fever-download
+#$ -V
+#$ -t 1-51
+#$ -cwd
+#$ -l rmem=10G
+module load apps/python/anaconda3-4.2.0
+source activate fever
+
+block=$SGE_TASK_ID
+PYTHONPATH=src S3_BUCKET=$bucket python src/jobs/download_dataset.py $block
