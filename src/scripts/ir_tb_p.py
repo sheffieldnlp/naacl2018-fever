ef61415e207c4b2cdf63adb4a7a2b9bf1ede4234
James Thorne <james@jthorne.co.uk>
2017-11-26 22:47:05 +0000

    compute % acc

---
+++ src/scripts/ir_tb_p.py
@@ -0,0 +1,37 @@
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--split', type=str)
+parser.add_argument('--count', type=int, default=1)
+args = parser.parse_args()
+
+split = args.split
+k = args.count
+
+def preprocess(p):
+    return p.replace(" ","_").replace("(","-LRB-").replace(")","-RRB-").replace(":","-COLON-")
+
+q = 0
+hits = 0
+
+with open("data/fever/fever.{0}.pages.p{1}.jsonl".format(split,k),"r") as f:
+
+    for idx,line in enumerate(f):
+
+        js = json.loads(line)
+        evidence = set([t[1] for t in js["evidence"] if isinstance(t,list) and len(t)>1])
+        predicted = [t[0] for t in js['predicted_pages']]
+        print(evidence)
+
+        print(predicted)
+
+
+        for p in evidence:
+            q += 1
+            if preprocess(p) in predicted:
+                hits+= 1
+
+
+
+    print(hits/q)
