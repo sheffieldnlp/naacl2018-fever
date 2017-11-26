85c7c838949d0a8ef8108a5fc07dd896aaaddb1c
James Thorne <james@jthorne.co.uk>
2017-11-26 22:58:17 +0000

    removed spurious print satatmenets

--- src/scripts/ir_tb_p.py
+++ src/scripts/ir_tb_p.py
@@ -22,9 +22,7 @@ with open("data/fever/fever.{0}.pages.p{1}.jsonl".format(split,k),"r") as f:
         js = json.loads(line)
         evidence = set([t[1] for t in js["evidence"] if isinstance(t,list) and len(t)>1])
         predicted = [t[0] for t in js['predicted_pages']]
-        print(evidence)
 
-        print(predicted)
 
 
         for p in evidence:
