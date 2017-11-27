82f4e143bb755797b7b8b873d48c8aa69a958d60
James Thorne <james@jthorne.co.uk>
2017-11-27 10:31:54 +0000

    get lines from doc db

---
+++ src/retrieval/fever_doc_db.py
@@ -0,0 +1,18 @@
+from drqa.retriever import DocDB, utils
+
+
+class FeverDocDB(DocDB):
+
+    def __init__(self,path=None):
+        super().__init__(path)
+
+    def get_doc_lines(self, doc_id):
+        """Fetch the raw text of the doc for 'doc_id'."""
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "SELECT lines FROM documents WHERE id = ?",
+            (utils.normalize(doc_id),)
+        )
+        result = cursor.fetchone()
+        cursor.close()
+        return result if result is None else result[0]
