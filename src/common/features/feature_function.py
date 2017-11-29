1ea30310eae4323521a687fba822d54f30ed5b96
James Thorne <james@jthorne.co.uk>
2017-11-29 11:32:30 +0000

    caching of features

--- src/common/features/feature_function.py
+++ src/common/features/feature_function.py
@@ -1,18 +1,70 @@
 import numpy as np
-
-
+import os
+import pickle
 class Features():
-    def __init__(self,features=list(),label_name="label"):
+    def __init__(self,features=list(),label_name="label",base_path="features"):
         self.feature_functions = features
         self.vocabs = dict()
         self.label_name = label_name
+        self.base_path = base_path
 
-    def load(self,dataset):
+
+    def load(self,train,dev=None,test=None):
+        train_fs = []
+        dev_fs = []
+        test_fs = []
+
+        for ff in self.feature_functions:
+            ffpath = os.path.join(self.base_path, type(ff).__name__)
+
+            if not os.path.exists(ffpath):
+                os.makedirs(ffpath)
+
+            #If we need train/dev/test data and these don't exist, we have to recreate the features
+            if (not os.path.exists(os.path.join(ffpath,"train"))) \
+                or (dev is not None and not os.path.exists(os.path.join(ffpath,"dev"))) \
+                or (test is not None and not os.path.exists(os.path.join(ffpath, "test"))):
+
+                self.inform(train,dev,test)
+
+            train_fs.append(self.generate_or_load(ff, train, "train"))
+            dev_fs.append(self.generate_or_load(ff, dev, "dev"))
+            test_fs.append(self.generate_or_load(ff, test, "test"))
+
+
+        return self.out(train_fs,train.data), self.out(dev_fs,dev.data), self.out(test_fs,test.data)
+
+    def out(self,features,ds):
+        if ds is not None:
+            return np.hstack(features) if len(features) > 1 else features, self.labels(ds)
+        return [[]],[]
+
+    def generate_or_load(self,feature,dataset,name):
+        ffpath = os.path.join(self.base_path, type(feature).__name__)
+
+        if dataset is not None:
+            if os.path.exists(os.path.join(ffpath,name)):
+                print("Loading Features for {0}.{1}".format(feature, name))
+                with open(os.path.join(ffpath, name), "rb") as f:
+                    features = pickle.load(f)
+
+            else:
+                print("Generating Features for {0}.{1}".format(feature,name))
+                features = feature.lookup(dataset.data)
+
+                with open(os.path.join(ffpath, name), "wb+") as f:
+                    pickle.dump(features, f)
+
+            return features
+
+        return None
+
+    def _load(self,dataset):
         fs = []
         for feature_function in self.feature_functions:
             print("Load {0}".format(feature_function))
-            fs.append(feature_function.lookup(dataset))
-        return np.hstack(fs) if len(fs)>1 else fs,self.labels(dataset)
+            fs.append(feature_function(dataset))
+        return np.hstack(fs) if len(fs) > 1 else fs, self.labels(dataset)
 
     def labels(self,data):
         return [datum[self.label_name] for datum in data]
--- src/rte/riedel/model.py
+++ src/rte/riedel/model.py
@@ -51,4 +51,5 @@ if __name__ == "__main__":
     dev.read()
     test.read()
 
-    f.inform(train,dev,test)
+    train_feats, dev_feats, test_feats = f.load(train,dev,test)
+    print(train_feats)
