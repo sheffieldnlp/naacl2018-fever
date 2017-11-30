93455919a16cd36cac3308b3972fde8f1b905cfa
James Thorne <james@jthorne.co.uk>
2017-11-30 00:31:00 +0000

    make model more similar to riedel's

--- src/rte/riedel/model.py
+++ src/rte/riedel/model.py
@@ -1,23 +1,22 @@
 from torch import nn
 
 class SimpleMLP(nn.Module):
-    def __init__(self,input_dim,hidden_dim,output_dim,dropout_p=.6):
+    def __init__(self,input_dim,hidden_dim,output_dim,keep_p=.6):
         super(SimpleMLP, self).__init__()
         self.fc1 = nn.Linear(input_dim,hidden_dim)
         self.fc2 = nn.Linear(hidden_dim,output_dim)
 
-        self.do = nn.Dropout(dropout_p)
+        self.do = nn.Dropout(1-keep_p)
         self.relu = nn.ReLU()
 
     def forward(self,x):
-        x = self.do(x)
+
         x = self.fc1(x)
-
         x = self.relu(x)
-
         x = self.do(x)
-        x = self.fc2(x)
 
+        x = self.fc2(x)
+        x = self.do(x)
         return x
 
 
