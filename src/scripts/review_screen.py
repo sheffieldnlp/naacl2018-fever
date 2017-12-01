930975c1de4fd7795b8002f2d2f7a2d426be8b03
James Thorne <james@jthorne.co.uk>
2017-12-01 17:40:42 +0000

    working annotation screen

--- src/scripts/review_screen.py
+++ src/scripts/review_screen.py
@@ -1,54 +1,57 @@
-import curses
 import json
 import sys
-from curses.textpad import rectangle
-
-#source activate pytorch; cd Documents/PhD/projects/10\ FEVER/fever-baselines; python src/scripts/review_screen.py data/dump1.json
-
-def render(item):
-    stdscr.addstr(2,3,"Claim:")
-    stdscr.addstr(2,10, item["text"])
-    rectangle(stdscr,1,1,3,100)
+import os
+#python review_screen.py dump100.json
 
 
-    stdscr.addstr(4,3,"Entity:")
-    stdscr.addstr(4,10,item["page"])
+def pprint(dump,hl=None):
 
-    stdscr.addstr(4,51,"Oracle:")
-    stdscr.addstr(4,58,str(item["isOracle"]))
+    for idx, line in enumerate(dump.split("\n")):
+        bts = line.split("\t")
+        print("{2}\t{0}\t{1}".format(bts[0],bts[1] if len(bts)>1 else "","*" if hl is not None and int(hl) == idx else ""))
 
-    stdscr.addstr(4, 76, "Reval:")
-    stdscr.addstr(4, 82, str(item["isReval"]))
-
-    rectangle(stdscr,3,1,5,48)
-    rectangle(stdscr, 3, 49, 5, 73)
-    rectangle(stdscr, 3, 74, 5, 100)
-
-    stdscr.addstr(6,3,"Annotation:")
-    stdscr.addstr(6,13,item["verifiable"])
-
-    stdscr.addstr(6,43,"Label:")
-    stdscr.addstr(6,53,item["label"] if "label" in item else "")
-
-    rectangle(stdscr, 5, 1, 7, 41)
-    rectangle(stdscr, 5, 42, 7, 100)
-
-
-
-    stdscr.refresh()
 
 # Read dump
 with open(sys.argv[1],"r") as f:
     data = json.load(f)
 
-# Make curses
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
+
+with open("notes.jsonl","a+") as f:
+    for item in data["annotations"]:
+        os.system("clear")
+
+        print("ID: \t{0}\t\t\t{1}".format(item["id"],item["text"]))
+        print("Original Page: \t{0}\t\t\tIs Oracle: {1}\t\t\tIs Reval: {2}".format(item["original_page"], item["isOracle"], item["isReval"]))
+        print("")
 
 
+        for idx,annotation in enumerate(item["annotations"]):
+            print("Annotation {0}".format(idx+1))
+            print("\tPage: {0}\t\tLine: {1}".format(annotation["page"], annotation["line"]))
+            print("\t{0}\t{1}\t\t(isOracleMaster: {2})".format(annotation["verifiable"],annotation["label"],annotation["isOracleMaster"]))
 
-for item in data:
-    render(item)
-    break
+            print("")
+            print("Selected Text")
+            pprint(data["texts"][annotation["page"]],annotation["line"])
+
+        print("")
+        print("")
+
+        print("Original Page:")
+
+        pprint(data["texts"][item["original_page"]])
+
+
+        print("")
+        print("")
+
+        correct = input("Correct annotation (y/n/q/s): ")
+        if correct=="q":
+            break
+        elif correct=="s":
+            continue
+
+        notes = input("Notes: ")
+
+        f.write(json.dumps({"id":item['id'],"correct":correct,"notes":notes})+"\n")
 
