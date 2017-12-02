813b152c15067cb932805e2d9693cd495ff53576
James Thorne <james@jthorne.co.uk>
2017-12-02 22:16:42 +0000

    namescheme

--- src/scripts/rte/fever_riedel_3way.py
+++ src/scripts/rte/fever_riedel_3way.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     db = FeverDocDB("data/fever/drqa.db")
     idx = set(db.get_doc_ids())
 
-    f = Features([TermFrequencyFeatureFunction(db)])
+    f = Features([TermFrequencyFeatureFunction(db,naming="3way_rand_ns")])
     jlr = JSONLineReader()
 
     formatter = FEVERGoldFormatter(idx, FEVERLabelSchema())
