b89b8f16d7fdc8cf177f2ddb421a761167b18481
James Thorne <james@jthorne.co.uk>
2017-12-02 23:00:12 +0000

    sample page for NEI from drqa

---
+++ src/scripts/retrieval/batch_ir_ns.py
@@ -0,0 +1,43 @@
+import argparse
+import json
+from tqdm import tqdm
+from drqa import retriever
+from drqa.retriever import DocDB
+
+
+def process(ranker, query, k=1):
+    doc_names, doc_scores = ranker.closest_docs(query, k)
+
+    return doc_names
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default=None)
+    parser.add_argument('--split',type=str)
+    parser.add_argument('--count',type=int, default=1)
+    args = parser.parse_args()
+
+    k = args.count
+    split = args.split
+    ranker = retriever.get_class('tfidf')(tfidf_path=args.model)
+
+    with open("data/fever/fever.{0}.jsonl".format(split),"r") as f:
+        with open("data/fever/fever.{0}.ns.pages.p{1}.jsonl".format(split,k),"w+") as f2:
+            for line in tqdm(f.readlines()):
+                line = json.loads(line)
+
+                if isinstance(line["evidence"][0], int):
+
+                    pages = process(ranker, line['claim'], k=k)
+                    pp = list(pages)
+
+                    ev = []
+                    for idx,e in enumerate(line['evidence']):
+                        if idx < len(pp):
+                            ev.append((e, pp[idx]))
+                    line['evidence'] = ev
+                f2.write(json.dumps(line) + "\n")
+
+
