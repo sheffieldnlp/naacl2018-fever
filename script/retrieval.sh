076dda75baf3ab9a2659011cc00a3bdfb754ea9a
James Thorne <james@jthorne.co.uk>
2017-12-08 00:14:11 +0000

    dont generate batch ir for training set

--- script/retrieval.sh
+++ script/retrieval.sh
@@ -1,19 +1,14 @@
 #Predict pages for each data split using DrQA
 
 #Top 1 page
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split train
 PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split dev
 PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split test
 
-
 #Top 5 pages
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split train
 PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split dev
 PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split test
 
-
 #Top 100 pages
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 100 --split train
 PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 100 --split dev
 PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 100 --split test
 
