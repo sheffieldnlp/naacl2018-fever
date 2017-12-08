b25d763e5e4fbf414181f07361361769d7cebf7d
James Thorne <james@jthorne.co.uk>
2017-12-08 11:49:58 +0000

    fix bug in neg

--- src/scripts/dataset/neg_sample_evidence.py
+++ src/scripts/dataset/neg_sample_evidence.py
@@ -18,7 +18,7 @@ r = random.Random(SimpleRandom.get_seed())
 
 with open("data/fever/test.ns.rand.jsonl", "w+") as f:
     for line in jlr.read("data/fever-data/test.jsonl"):
-        if line["VERIFIABLE"] == "NOT ENOUGH INFO":
+        if line["verifiable"] == "NOT ENOUGH INFO":
             ev = []
             for e in line['evidence']:
                 ev.append((e[0], idx[r.next_rand(0, len(idx))], -1))
@@ -27,7 +27,7 @@ with open("data/fever/test.ns.rand.jsonl", "w+") as f:
 
 with open("data/fever/dev.ns.rand.jsonl", "w+") as f:
     for line in jlr.read("data/fever-data/dev.jsonl"):
-        if line["VERIFIABLE"]=="NOT ENOUGH INFO":
+        if line["verifiable"]=="NOT ENOUGH INFO":
             ev = []
             for e in line['evidence']:
                 ev.append((e[0],idx[r.next_rand(0,len(idx))],-1))
@@ -39,7 +39,7 @@ with open("data/fever/dev.ns.rand.jsonl", "w+") as f:
 
 with open("data/fever/train.ns.rand.jsonl", "w+") as f:
     for line in jlr.read("data/fever-data/train.jsonl"):
-        if line["VERIFIABLE"] == "NOT ENOUGH INFO":
+        if line["verifiable"] == "NOT ENOUGH INFO":
             ev = []
             for e in line['evidence']:
                 ev.append((e[0], idx[r.next_rand(0, len(idx))], -1))
