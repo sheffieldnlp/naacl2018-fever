1822125a2e6e72d64041802e94086b21ff9ec0ed
James Thorne <james@jthorne.co.uk>
2017-12-08 11:49:15 +0000

    randomisation for negative sampling

--- src/scripts/dataset/neg_sample_evidence.py
+++ src/scripts/dataset/neg_sample_evidence.py
@@ -1,8 +1,9 @@
 from common.dataset.reader import JSONLineReader
+from common.util import random
 from common.util.random import SimpleRandom
 from retrieval.fever_doc_db import FeverDocDB
 import json
-
+import random
 
 jlr = JSONLineReader()
 
@@ -10,9 +11,19 @@ docdb = FeverDocDB("data/fever/drqa.db")
 
 idx = docdb.get_doc_ids()
 
-r = SimpleRandom.get_instance()
+SimpleRandom.set_seeds()
+
+r = random.Random(SimpleRandom.get_seed())
 
 
+with open("data/fever/test.ns.rand.jsonl", "w+") as f:
+    for line in jlr.read("data/fever-data/test.jsonl"):
+        if line["VERIFIABLE"] == "NOT ENOUGH INFO":
+            ev = []
+            for e in line['evidence']:
+                ev.append((e[0], idx[r.next_rand(0, len(idx))], -1))
+            line['evidence'] = ev
+        f.write(json.dumps(line)+"\n")
 
 with open("data/fever/dev.ns.rand.jsonl", "w+") as f:
     for line in jlr.read("data/fever-data/dev.jsonl"):
@@ -28,16 +39,6 @@ with open("data/fever/dev.ns.rand.jsonl", "w+") as f:
 
 with open("data/fever/train.ns.rand.jsonl", "w+") as f:
     for line in jlr.read("data/fever-data/train.jsonl"):
-        if line["VERIFIABLE"] == "NOT ENOUGH INFO":
-            ev = []
-            for e in line['evidence']:
-                ev.append((e[0], idx[r.next_rand(0, len(idx))], -1))
-            line['evidence'] = ev
-        f.write(json.dumps(line)+"\n")
-
-
-with open("data/fever/test.ns.rand.jsonl", "w+") as f:
-    for line in jlr.read("data/fever-data/test.jsonl"):
         if line["VERIFIABLE"] == "NOT ENOUGH INFO":
             ev = []
             for e in line['evidence']:
