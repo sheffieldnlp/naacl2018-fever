082c886413766bc8dbd35b612e3ea4a47aa34b9b
James Thorne <james@jthorne.co.uk>
2017-12-11 16:49:43 +0000

    fix error in job script

--- script/ir_bidaf.sh
+++ script/ir_bidaf.sh
@@ -3,7 +3,7 @@
 #$ -cwd
 #$ -l rmem=16G
 #$ -l gpu=1
--l h_rt= 95:00:00
+#$ -l h_rt= 95:00:00
 module load libs/CUDA/8.0.44/binary
 source activate fever
 PYTHONPATH=src:lib/DrQA:lib/allennlp python src/scripts/retrieval/sentence/train.py data/fever/fever.db config/fever_bidaf.json logs/bidaf
