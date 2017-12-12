668d036bd70c3ac43547b305f42d3736622d5ca6
James Thorne <james@jthorne.co.uk>
2017-12-12 16:11:38 +0000

    retrieval script

--- script/retrieval.sh
+++ script/retrieval.sh
@@ -21,13 +21,13 @@ PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir.py  --model data/f
 PYTHONPATH=src:lib/DrQA python src/scripts/dataset/neg_sample_evidence.py
 
 #Select top 5 pages for training not enough info
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir_ns.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split train
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir_ns.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split train
+PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split train
+PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split train
 
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir_ns.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split dev
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir_ns.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split dev
+PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split dev
+PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split dev
 
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir_ns.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split test
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/batch_ir_ns.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split test
+PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split test
+PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/drqatfidf/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split test
 
 
