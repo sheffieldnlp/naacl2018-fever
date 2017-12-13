ae0aba12382e82deb4988c589731a9f1c663c93a
James Thorne <james@jthorne.co.uk>
2017-12-13 15:02:17 +0000

    fever path

--- script/retrieval.sh
+++ script/retrieval.sh
@@ -21,8 +21,8 @@ PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir.py  --mod
 PYTHONPATH=src:lib/DrQA python src/scripts/dataset/neg_sample_evidence.py > logs/ir.nse &
 
 #Select top 5 pages for training not enough info
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split train > logs/ir.ns.p5.train &
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/drqa-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split train > logs/ir.ns.p1.train &
+PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split train > logs/ir.ns.p5.train &
+PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split train > logs/ir.ns.p1.train &
 
 PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split dev > logs/ir.ns.p5.dev &
 PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split dev > logs/ir.ns.p1.dev &
