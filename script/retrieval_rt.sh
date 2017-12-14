bd4537a4eb6d2dad49dfded9f73a8b138db7b6fb
James Thorne <james@jthorne.co.uk>
2017-12-14 23:30:39 +0000

    runtime page retrieval

--- script/retrieval_rt.sh
+++ script/retrieval_rt.sh
@@ -12,24 +12,11 @@ PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir.py  --mod
 PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 10 --split dev > logs/ir.dev.10 &
 PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 10 --split test > logs/ir.test.10 &
 
-#Top 100 pages
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 100 --split dev > logs/ir.dev.100 &
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 100 --split test > logs/ir.test.100 &
-
+#Top 25 pages
 PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 25 --split dev > logs/ir.dev.25 &
 PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 25 --split test > logs/ir.test.25 &
 
-#Sample random pages for training not enough info
-PYTHONPATH=src:lib/DrQA python src/scripts/dataset/neg_sample_evidence.py > logs/ir.nse &
-
-#Select top 5 pages for training not enough info
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split train > logs/ir.ns.p5.train &
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split train > logs/ir.ns.p1.train &
-
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split dev > logs/ir.ns.p5.dev &
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split dev > logs/ir.ns.p1.dev &
-
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 5 --split test > logs/ir.ns.p5.test &
-PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir_ns.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 1 --split test > logs/ir.ns.p1.test &
-
+#Top 100 pages
+PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 100 --split dev > logs/ir.dev.100 &
+PYTHONPATH=src:lib/DrQA python src/scripts/retrieval/document/batch_ir.py  --model data/fever/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz --count 100 --split test > logs/ir.test.100 &
 
