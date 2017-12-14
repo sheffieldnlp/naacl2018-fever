58c2119751e712c7b437fa202a10e2c994d4e7ed
James Thorne <james@jthorne.co.uk>
2017-12-14 16:32:49 +0000

    added run scripts for all expts

---
+++ script/sharc/rte_doc_lstm_da_nn.sh
@@ -0,0 +1,18 @@
+#$ -N fever-doc-lstm-da-nn
+#$ -V
+#$ -cwd
+#$ -q rse.q
+#$ -P rse
+#$ -l rmem=40G
+#$ -l gpu=1
+#$ -l h_rt=95:00:00
+
+source /etc/profile.d/modules.sh
+source ~/.bashrc
+
+module load libs/CUDA/8.0.44/binary
+
+source activate dgx
+
+cp data/fever/fever.db $TMPDIR/fever.db
+PYTHONPATH=src:lib/DrQA/:lib/allennlp/ python src/scripts/rte/da/train_da.py $TMPDIR/fever.db config/fever_nn_ora.json logs/doc_nn
---
+++ script/sharc/rte_doc_mlp_rs.sh
@@ -0,0 +1,18 @@
+#$ -N fever-doc-mlp-rs
+#$ -V
+#$ -cwd
+#$ -q rse.q
+#$ -P rse
+#$ -l rmem=22G
+#$ -l gpu=1
+#$ -l h_rt=95:00:00
+
+source /etc/profile.d/modules.sh
+source ~/.bashrc
+
+module load libs/CUDA/8.0.44/binary
+
+source activate dgx
+
+cp data/fever/fever.db $TMPDIR/fever.db
+TRAIN=1 PYTHONPATH=src:lib/DrQA/:lib/allennlp/ python src/scripts/rte/mlp/document_mlp.py $TMPDIR/fever.db data/fever/train.ns.rand.jsonl data/fever/dev.ns.rand.jsonl --model ns_rs_doc
