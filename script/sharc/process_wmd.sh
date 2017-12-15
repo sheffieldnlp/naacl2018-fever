96514598279c2a3a80781e409e8c87a2841941d1
James Thorne <james@jthorne.co.uk>
2017-12-15 11:32:59 +0000

    download spacy vocab

--- script/sharc/process_wmd.sh
+++ script/sharc/process_wmd.sh
@@ -13,4 +13,5 @@ source ~/.bashrc
 source activate dgx
 
 cp data/fever/fever.db $TMPDIR/fever.db
+python -m spacy download en
 PYTHONPATH=src:lib/DrQA/:lib/allennlp/ python src/scripts/retrieval/sentence/process.py $TMPDIR/fever.db data/fever/dev.pages.p100.jsonl data/fever/dev.sentences.${k}.${l} ${k} ${l}
