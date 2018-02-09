c4b2ab8cc87137899bfafa349cf2976ae04b9830
James Thorne <james@jthorne.co.uk>
2018-02-09 20:34:04 +0000

    differnet filtering for list_of pages

--- src/scripts/filter_uninformative.py
+++ src/scripts/filter_uninformative.py
@@ -1,7 +1,11 @@
 import re
 
 def uninformative(title):
-    return  '-LRB-disambiguation-RRB-' in title.lower() or '-LRB-disambiguation_page-RRB-' in title.lower() or re.match(r'(List_of_.+)|(Index_of_.+)|(Outline_of_.+)',  title)
+    return  '-LRB-disambiguation-RRB-' in title.lower() \
+            or '-LRB-disambiguation_page-RRB-' in title.lower() \
+            or title.lower().startswith('list_of_') \
+            or title.lower().startswith('index_of_.') \
+            or title.lower().startswith('outline_of_')
 
 def preprocess(doc):
     return doc if not uninformative(doc['id']) else None
