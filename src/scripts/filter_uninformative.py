d361b606281b408625891568ae57324707039ff0
James Thorne <james@jthorne.co.uk>
2018-02-10 11:45:22 +0000

    also handle pages which are lists of lists

--- src/scripts/filter_uninformative.py
+++ src/scripts/filter_uninformative.py
@@ -2,6 +2,7 @@ def uninformative(title):
     return  '-LRB-disambiguation-RRB-' in title.lower() \
             or '-LRB-disambiguation_page-RRB-' in title.lower() \
             or title.lower().startswith('list_of_') \
+            or title.lower().startswith("lists_of_") \
             or title.lower().startswith('index_of_.') \
             or title.lower().startswith('outline_of_')
 
