13e8286091d55474ce5dd1de4d0c28763993ed92
James Thorne <james@jthorne.co.uk>
2018-02-26 21:11:14 +0000

    filename

--- script/train_mlp.sh
+++ script/train_mlp.sh
@@ -4,5 +4,5 @@ do
     echo "Train $f";
     export GPU=1
     export CUDA_DEVICE=0
-    PYTHONPATH=src python src/scripts/rte/mlp/train_mlp.py data/fever/fever.db $f data/fever/dev.ns.pages.p1.jsonl --model ablation$f --sentence true
+    PYTHONPATH=src python src/scripts/rte/mlp/train_mlp.py data/fever/fever.db data/ablation/$f data/fever/dev.ns.pages.p1.jsonl --model ablation$f --sentence true
 done
