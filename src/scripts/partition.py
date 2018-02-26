d49a439993bd031fb9e7af0af683145d8c85d3fe
James Thorne <james@jthorne.co.uk>
2018-02-26 20:17:18 +0000

    split ceil

--- src/scripts/partition.py
+++ src/scripts/partition.py
@@ -1,5 +1,5 @@
 import argparse
-
+import math
 parser = argparse.ArgumentParser()
 parser.add_argument('--in_file', type=str)
 parser.add_argument('--out_file', type=str)
@@ -9,7 +9,7 @@ args = parser.parse_args()
 
 with open(args.in_file,"r") as in_file:
     lines = in_file.readlines()
-keep = lines[:len(lines)*args.split]
+keep = lines[:math.ceil(len(lines) * args.split)]
 
 with open(args.out_file,"w+") as out_file:
     out_file.writelines(keep)
