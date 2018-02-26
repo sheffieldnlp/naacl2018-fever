542cc9f66d22dbddda693633d354c683a1d5a33d
James Thorne <james@jthorne.co.uk>
2018-02-26 20:16:20 +0000

    float split

--- src/scripts/partition.py
+++ src/scripts/partition.py
@@ -3,13 +3,13 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--in_file', type=str)
 parser.add_argument('--out_file', type=str)
-parser.add_argument('--split', type=int)
+parser.add_argument('--split', type=float)
 
 args = parser.parse_args()
 
-with open(args.in_file) as in_file:
+with open(args.in_file,"r") as in_file:
     lines = in_file.readlines()
-keep = lines[:len(lines)*100//args.split]
+keep = lines[:len(lines)*args.split]
 
-with open(args.out_file) as out_file:
+with open(args.out_file,"w+") as out_file:
     out_file.writelines(keep)
