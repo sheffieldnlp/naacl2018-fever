711777e9bd48d5cee33553ad4e24bfdba7b13238
James Thorne <james@jthorne.co.uk>
2018-03-01 18:22:54 +0000

    test multiple seeds

--- script/train_lstm.sh
+++ script/train_lstm.sh
@@ -4,5 +4,9 @@ do
     echo "Train $f";
     export CUDA_DEVICE=0 #or cuda device id
 
-    PYTHONPATH=src python src/scripts/rte/da/train_da.py data/fever/fever.db config/fever_nn_ora_sent.json logs/ablation/$f --cuda-device $CUDA_DEVICE --overrides {"train_data_path":"data/ablation/$f"}
+    RANDOM_SEED=1234 PYTHONPATH=src python src/scripts/rte/da/train_da.py data/fever/fever.db config/fever_nn_ora_sent.json logs/ablation/seed1-$f --cuda-device $CUDA_DEVICE --overrides {"train_data_path":"data/ablation/$f"}
+    RANDOM_SEED=2345 PYTHONPATH=src python src/scripts/rte/da/train_da.py data/fever/fever.db config/fever_nn_ora_sent.json logs/ablation/seed2-$f --cuda-device $CUDA_DEVICE --overrides {"train_data_path":"data/ablation/$f"}
+    RANDOM_SEED=3456 PYTHONPATH=src python src/scripts/rte/da/train_da.py data/fever/fever.db config/fever_nn_ora_sent.json logs/ablation/seed3-$f --cuda-device $CUDA_DEVICE --overrides {"train_data_path":"data/ablation/$f"}
+    RANDOM_SEED=4567 PYTHONPATH=src python src/scripts/rte/da/train_da.py data/fever/fever.db config/fever_nn_ora_sent.json logs/ablation/seed4-$f --cuda-device $CUDA_DEVICE --overrides {"train_data_path":"data/ablation/$f"}
+    RANDOM_SEED=5678 PYTHONPATH=src python src/scripts/rte/da/train_da.py data/fever/fever.db config/fever_nn_ora_sent.json logs/ablation/seed5-$f --cuda-device $CUDA_DEVICE --overrides {"train_data_path":"data/ablation/$f"}
 done
