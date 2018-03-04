ca9ecfb4e0a40956c70d50b3f202497d3fadf02e
James Thorne <james@jthorne.co.uk>
2018-03-04 00:17:24 +0000

    eval all seeds

--- script/eval_lstm.sh
+++ script/eval_lstm.sh
@@ -4,5 +4,9 @@ do
     echo "Eval $f";
     export GPU=1
     export CUDA_DEVICE=0
-    PYTHONPATH=src python src/scripts/rte/da/eval_da.py data/fever/fever.db logs/ablation/$f/model.tar.gz data/fever/dev.ns.pages.p1.jsonl
+    PYTHONPATH=src python src/scripts/rte/da/eval_da.py data/fever/fever.db logs/ablation/seed1-$f/model.tar.gz data/fever/dev.ns.pages.p1.jsonl
+    PYTHONPATH=src python src/scripts/rte/da/eval_da.py data/fever/fever.db logs/ablation/seed2-$f/model.tar.gz data/fever/dev.ns.pages.p1.jsonl
+    PYTHONPATH=src python src/scripts/rte/da/eval_da.py data/fever/fever.db logs/ablation/seed3-$f/model.tar.gz data/fever/dev.ns.pages.p1.jsonl
+    PYTHONPATH=src python src/scripts/rte/da/eval_da.py data/fever/fever.db logs/ablation/seed4-$f/model.tar.gz data/fever/dev.ns.pages.p1.jsonl
+    PYTHONPATH=src python src/scripts/rte/da/eval_da.py data/fever/fever.db logs/ablation/seed5-$f/model.tar.gz data/fever/dev.ns.pages.p1.jsonl
 done
--- script/eval_mlp.sh
+++ script/eval_mlp.sh
@@ -4,5 +4,9 @@ do
     echo "Eval $f";
     export GPU=1
     export CUDA_DEVICE=0
-    PYTHONPATH=src python src/scripts/rte/mlp/eval_mlp.py data/fever/fever.db data/fever/dev.ns.pages.p1.jsonl --model ablation$f --sentence true
+    PYTHONPATH=src python src/scripts/rte/mlp/eval_mlp.py data/fever/fever.db data/fever/dev.ns.pages.p1.jsonl --model ablation-seed1-$f --sentence true
+    PYTHONPATH=src python src/scripts/rte/mlp/eval_mlp.py data/fever/fever.db data/fever/dev.ns.pages.p1.jsonl --model ablation-seed2-$f --sentence true
+    PYTHONPATH=src python src/scripts/rte/mlp/eval_mlp.py data/fever/fever.db data/fever/dev.ns.pages.p1.jsonl --model ablation-seed3-$f --sentence true
+    PYTHONPATH=src python src/scripts/rte/mlp/eval_mlp.py data/fever/fever.db data/fever/dev.ns.pages.p1.jsonl --model ablation-seed4-$f --sentence true
+    PYTHONPATH=src python src/scripts/rte/mlp/eval_mlp.py data/fever/fever.db data/fever/dev.ns.pages.p1.jsonl --model ablation-seed5-$f --sentence true
 done
