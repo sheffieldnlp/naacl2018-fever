de8b36b5f084c8cceeb7e1b935dff2ea3348dd42
James Thorne <james@jthorne.co.uk>
2018-03-07 14:05:24 +0000

    snli predicter

--- src/rte/parikh/predictor.py
+++ src/rte/parikh/predictor.py
@@ -6,6 +6,8 @@ from allennlp.common import JsonDict
 from allennlp.data import Instance
 from allennlp.service.predictors import Predictor
 
+from common.util.random import SimpleRandom
+
 
 @Predictor.register('drwiki-te')
 class TextualEntailmentPredictor(Predictor):
@@ -19,11 +21,21 @@ class TextualEntailmentPredictor(Predictor):
     def set_docdb(self,db):
         self.db = db
 
+
+    def get_doc_line(self,doc,line):
+        lines = self.db.get_doc_lines(doc)
+        if line > -1:
+            return lines.split("\n")[line].split("\t")[1]
+        else:
+            non_empty_lines = [line.split("\t")[1] for line in lines.split("\n") if len(line.split("\t"))>1 and len(line.split("\t")[1].strip())]
+            return non_empty_lines[SimpleRandom.get_instance().next_rand(0,len(non_empty_lines)-1)]
+
+
     def _json_to_instances(self,json):
         hypothesis_text = json["claim"]
         instances = []
-        for page,score in json["predicted_pages"]:
-            premise_text = self.db.get_doc_text(page)
+        for _,_, page,sentence in json["evidence"]:
+            premise_text = self.get_doc_line(page,sentence)
             instances.append(self._dataset_reader.text_to_instance(premise_text, hypothesis_text))
         return instances
 
