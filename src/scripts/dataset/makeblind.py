30239064358195de3a0b40a41a32e93969026ec7
James Thorne <james@jthorne.co.uk>
2018-04-09 13:06:05 +0100

    used to make blind split

---
+++ src/scripts/dataset/makeblind.py
@@ -0,0 +1,13 @@
+import json
+
+examples = []
+with open("data/fever-data/shared_task_dev.jsonl") as input_data:
+    for line in input_data:
+        examples.append(json.loads(line))
+
+
+
+with open("data/fever-data/example_blind_public.jsonl","w+") as public, open("data/fever-data/example_blind_private.jsonl","w+") as private:
+    for line in examples:
+        public.write(json.dumps({"id": line['id'], "claim": line["claim"]})+"\n")
+        private.write(json.dumps({"id": line['id'], "evidence": line["evidence"],"all_evidence":line["all_evidence"],"label":line["label"]})+"\n")
