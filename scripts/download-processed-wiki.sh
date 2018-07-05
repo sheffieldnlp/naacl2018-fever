a50057123d739cf6cbd600f24949bc49bc7275f0
James Thorne <james@jthorne.co.uk>
2018-07-05 14:11:08 +0100

    fix preprocessed tfidf index download location

--- scripts/download-processed-wiki.sh
+++ scripts/download-processed-wiki.sh
@@ -3,5 +3,5 @@ mkdir -p data
 mkdir -p data/index
 mkdir -p data/fever
 
-wget -O data/index/fever-tfidf-ngram=D2-hash=16777216-tokenizer=simple.npz https://s3-eu-west-1.amazonaws.com/fever.public/wiki_index/fever-tfidf-ngram%3D2-hash%3D16777216-tokenizer%3Dsimple.npz
+wget -O data/index/fever-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz https://s3-eu-west-1.amazonaws.com/fever.public/wiki_index/fever-tfidf-ngram%3D2-hash%3D16777216-tokenizer%3Dsimple.npz
 wget -O data/fever/fever.db https://s3-eu-west-1.amazonaws.com/fever.public/wiki_index/fever.db
