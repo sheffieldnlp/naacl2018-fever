ae9e72db4b7fe0d51593d5e943639adce5f6694d
James Thorne <james@jthorne.co.uk>
2018-07-06 09:28:38 +0100

    fix shared task data locations

--- scripts/download-data.sh
+++ scripts/download-data.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mkdir -p data
 mkdir -p data/fever-data
-wget -O data/fever-data/train.jsonl https://s3-eu-west-1.amazonaws.com/fever.public/shared_task_train.jsonl
+wget -O data/fever-data/train.jsonl https://s3-eu-west-1.amazonaws.com/fever.public/train.jsonl
 wget -O data/fever-data/dev.jsonl https://s3-eu-west-1.amazonaws.com/fever.public/shared_task_dev.jsonl
-wget -O data/fever-data/test.jsonl https://s3-eu-west-1.amazonaws.com/fever.public/shared_task_test.jsonl
---
+++ scripts/download-shared-task-test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -O data/fever-data/test.jsonl https://s3-eu-west-1.amazonaws.com/fever.public/shared_task_test.jsonl
