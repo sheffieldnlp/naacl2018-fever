c142af5ad8bb458c429f7b2c5eed3093108db419
mithun <mithunpaul08@gmail.com>
2018-07-08 10:45:16 -0700

    fixed another syntax

--- src/common/util/log_helper.py
+++ src/common/util/log_helper.py
@@ -5,15 +5,15 @@ class LogHelper():
     @staticmethod
     def setup():
         logging.basicConfig(level=logging.WARNING,
-                            FORMAT='[%(levelname)s] %(asctime)s - %(name)s - %(message)s',
+                            format='[%(levelname)s] %(asctime)s - %(name)s - %(message)s',
                             datefmt='%m-%d %H:%M',
                             filename='log_fever.log',
                             filemode='w')
         LogHelper.handler = logging.StreamHandler()
         LogHelper.handler.setLevel(logging.DEBUG)
 
-        LogHelper.handler2 = logging.FileHandler(filename)
-        LogHelper.handler2.setLevel(logging.DEBUG)
+        # LogHelper.handler2 = logging.FileHandler(filename)
+        # LogHelper.handler2.setLevel(logging.DEBUG)
 
         LogHelper.get_logger(LogHelper.__name__).info("Log Helper set up")
 
