5f9950bd31f18367efc99666a7ac65ce3ccdc3b5
mithun <mithunpaul08@gmail.com>
2018-07-10 23:17:10 -0700

    changed warning to debug

--- src/rte/mithun/log.py
+++ src/rte/mithun/log.py
@@ -2,17 +2,17 @@ import logging
 
 def setup_custom_logger(name):
 
-    logging.basicConfig(level=logging.WARNING,
+    logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
-                    filename='log_from_log4j.txt',
+                    filename='log_fever.txt',
                     filemode='w')
 
     logger = logging.getLogger(name)
     '''critical, error > warning,info, debug'''
 
     ch=logging.StreamHandler()
-    ch.setLevel(logging.WARNING)
+    ch.setLevel(logging.DEBUG)
     logging.getLogger('').addHandler(ch)
 
 
