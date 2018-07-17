b36040130368728163ed41bc636d9b6425afb668
mithun <mithunpaul08@gmail.com>
2018-07-16 21:51:27 -0700

    new log file name

--- src/rte/mithun/log.py
+++ src/rte/mithun/log.py
@@ -5,7 +5,7 @@ def setup_custom_logger(name):
     logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
-                    filename='log_fever.txt',
+                    filename='log_fever_verb.txt',
                     filemode='w')
 
     logger = logging.getLogger(name)
