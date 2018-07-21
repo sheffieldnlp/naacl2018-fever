84ba5d17842ddc5f808f69978cddce1251b2b9da
mithun <mithunpaul08@gmail.com>
2018-07-21 16:21:11 -0700

    will exit printing index

--- src/rte/mithun/trainer.py
+++ src/rte/mithun/trainer.py
@@ -487,32 +487,32 @@ def negated_verbs_count(lemmatized_headline_split, headline_pos_split, lemmatize
                     logging.debug("found a verb which has same verb in headline and body")
                     logging.debug(index)
                     logging.debug(vb_head)
-                    logging.debug(lemmatized_headline_split)
-                    logging.debug(lemmatized_body_split)
                     sys.exit(1)
 
-        #
-        # # take that position value, go through dependency parse # and find if any of the leading edges go through "neg"
-        # for p in vb_positions_body:
-        #     logging.debug(p)
-        #     for edges in body_deps:
-        #             logging.debug(edges)
-        #             dest = edges["destination"]
-        #             src = edges["source"]
-        #             rel = edges["relation"]
-        #             logging.debug(src)
-        #             logging.debug(rel)
-        #             logging.debug(dest)
-        #
-        #             if ((p==src) and (rel=="neg")):
-        #                 logging.debug("found a verb having negative edge")
-        #                 logging.debug("lemmatized_headline_split")
-        #                 logging.debug(lemmatized_headline_split)
-        #                 logging.debug(src)
-        #                 logging.debug(rel)
-        #                 logging.debug(dest)
-        #                 # and find if any of the leading edges go through "neg"
-        #                 sys.exit(1)
+
+
+        # take that position value, go through dependency parse # and find if any of the leading edges go through "neg"
+        if(len(vb_positions_body)>0)
+            for p in vb_positions_body:
+                logging.debug(p)
+                for edges in body_deps:
+                        logging.debug(edges)
+                        dest = edges["destination"]
+                        src = edges["source"]
+                        rel = edges["relation"]
+                        logging.debug(src)
+                        logging.debug(rel)
+                        logging.debug(dest)
+
+                        if ((p==src) and (rel=="neg")):
+                            logging.debug("found a verb having negative edge")
+                            logging.debug(lemmatized_headline_split)
+                            logging.debug(lemmatized_body_split)
+                            logging.debug(src)
+                            logging.debug(rel)
+                            logging.debug(dest)
+                            # and find if any of the leading edges go through "neg"
+                            sys.exit(1)
 
 
         features = [0, 0]
