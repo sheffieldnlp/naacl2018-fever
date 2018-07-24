b402c1d8bf8f7fc9eb79016f8a381a4f2c1a0873
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2018-07-24 14:20:43 -0700

    only pos_head_neg_body

--- src/rte/mithun/trainer.py
+++ src/rte/mithun/trainer.py
@@ -579,9 +579,9 @@ def negated_verbs_count(lemmatized_headline_split, headline_pos_split, lemmatize
             logging.info("pos_head_neg_body>0")
             sys.exit(1)
 
-        if   neg_head_neg_body > 0:
-            logging.info("neg_head_neg_body>0")
-            sys.exit(1)
+        # if   neg_head_neg_body > 0:
+        #     logging.info("neg_head_neg_body>0")
+        #     sys.exit(1)
 
 
 
