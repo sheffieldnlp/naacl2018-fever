e1c1a41d264e34ba19d2b470c91490cd882eb856
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2018-07-25 17:47:29 -0700

    log debug to disk

--- src/rte/mithun/log.py
+++ src/rte/mithun/log.py
@@ -2,7 +2,7 @@ import logging
 
 def setup_custom_logger(name):
 
-    logging.basicConfig(level=logging.WARNING,
+    logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
                     filename='log_fever.txt',
