8061753a288d5e4d7aefc07b81d93c0fcf4d0a54
mithun <mithunpaul08@gmail.com>
2018-09-28 10:49:05 -0700

    will print entities.

--- src/retrieval/read_claims.py
+++ src/retrieval/read_claims.py
@@ -277,6 +277,12 @@ class UOFADataReader():
               out.write(doc2.to_JSON())
               out.write("\n")
 
+        print(f'doc1:{doc1}')
+        print(f'doc1.lemmas:{doc1.lemmas}')
+        print(f'doc1.entities:{doc1.entities}')'
+        print(f'doc1.sentences[0]:{doc1.sentences[0]}')
+        print(f'doc1.sentences[0].lemmas:{doc1.sentences[0].lemmas}')
+        print(f'doc1.sentences[0].entities:{doc1.sentences[0].entities}')
 
         return doc1.lemmas,doc2.lemmas
 
