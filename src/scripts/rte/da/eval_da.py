7bacd5dfc6e5c690e8349afc68fc2d66c4e02aaf
mithun <mithunpaul08@gmail.com>
2018-10-01 14:35:59 -0700

    run_name =dev

--- src/scripts/rte/da/eval_da.py
+++ src/scripts/rte/da/eval_da.py
@@ -40,7 +40,7 @@ def eval_model(db: FeverDocDB, args) -> Model:
                                  token_indexers=TokenIndexer.dict_from_params(ds_params.pop('token_indexers', {})))
 
     logger.info("Reading training data from %s", args.in_file)
-    data = reader.read(args.in_file).instances
+    data = reader.read(args.in_file,"dev").instances
 
     actual = []
     predicted = []
