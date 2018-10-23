65a1105e4a73f9581a62ae83792a2605586a9cfb
mithun <mithunpaul08@gmail.com>
2018-10-22 20:32:39 -0700

    should print 1st entry of non smart ner

--- src/rte/parikh/reader.py
+++ src/rte/parikh/reader.py
@@ -71,10 +71,9 @@ class FEVERReader(DatasetReader):
     @overrides
     def read(self, file_path: str, run_name,do_annotation):
         #logger.info("got inside read")
-        logging.info("got inside read")
-        #print("got inside read")
-        sys.exit(1)
-
+        #logging.info("got inside read")
+        print("got inside read")
+        
 
         instances = []
 
