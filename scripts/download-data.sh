3b50384d89af1df3ca88c8ca746697ef5132744d
James Thorne <james@jthorne.co.uk>
2023-02-03 13:23:40 +0900

    Update download-data.sh

--- scripts/download-data.sh
+++ scripts/download-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p data
 mkdir -p data/fever-data
-wget -O data/fever-data/train.jsonl https://s3-eu-west-1.amazonaws.com/fever.public/train.jsonl
-wget -O data/fever-data/dev.jsonl https://s3-eu-west-1.amazonaws.com/fever.public/shared_task_dev.jsonl
-wget -O data/fever-data/test.jsonl https://s3-eu-west-1.amazonaws.com/fever.public/shared_task_test.jsonl
+wget -O data/fever-data/train.jsonl https://fever.ai/download/fever/train.jsonl
+wget -O data/fever-data/dev.jsonl https://fever.ai/download/fever/shared_task_dev.jsonl
+wget -O data/fever-data/test.jsonl https://fever.ai/download/fever/shared_task_test.jsonl
